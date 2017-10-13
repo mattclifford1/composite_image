8ca6433805bcd15c35ae759b66ea91662760e69c
mattclifford1 <mattc863@hotmail.com>
2017-10-13 17:34:20 +0100

    shifting vector not working

--- im_cut.m
+++ im_cut.m
@@ -3,11 +3,15 @@ function cutIm =  im_cut(Im,V)
 if ischar(Im) == 1
     Im = imread(Im);  %read in image
 end
+
 s = size(Im);
 cut = zeros(s);
 cut((s(1)-V(1)):end,(s(1)-V(1)):end) = 1;
-cut = double(cut);
-Im = double(Im);
+size(Im)
+
+cut = uint8(cut);
+Im = uint8(Im); 
 cutIm = cut.*Im;
+
 
 
