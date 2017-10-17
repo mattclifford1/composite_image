87741f3f3c556a27503043f7699e35f4345ebed4
Zoe Webber <znz282@hotmail.co.uk>
2017-10-17 09:19:08 +0100

    fixed overlapping code

--- findFullName.m
+++ findFullName.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function fileName=findFullName(number)
 
 %cd dataSet  
@@ -15,22 +14,4 @@ for i =1:flTotalLength
     end
 end
 
-=======
-function fileName=findFullName(number)
-
-cd dataSet  
-
-fl= dir('*.tif');   %list of all files with .tif extension
-cd ..
-
-flTotalLength=length(fl);
-[finalMatrix,frameList]=readFiles();
-
-for i =1:flTotalLength
-    if frameList(i)==number
-        fileName=fl(i).name;
-    end
-end
-
->>>>>>> 901cfe53ee09bafc8da78bab70893f9bdd0bfaa0
 end
