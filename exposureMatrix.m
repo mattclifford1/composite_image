129539290e041b77cf94220f1a247b49c2ece184
unknown <tw15220@my.bristol.ac.uk>
2017-10-18 17:14:50 +0100

    matrix of exposure

---
+++ exposureMatrix.m
@@ -0,0 +1,17 @@
+function expoMatrix=exposureMatrix()
+%Outputs the matrix of exposure
+[finalMatrix,frameList]=readFiles();
+
+
+[row,col]=size(finalMatrix);
+%expoMatrix=ones(row,col);
+
+for i=1:row
+    for j=1:col
+        if finalMatrix(i,j)~=0
+            expoMatrix(i,j)=mean2(imread(strcat('./images/',findFullName(finalMatrix(i,j)))));
+        else
+            expoMatrix(i,j)=1;
+        end
+    end
+end
