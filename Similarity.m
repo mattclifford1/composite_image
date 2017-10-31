e0c5392344e94a8e42e5d0fe56ed740ddb62fdc6
Zoe Webber <znz282@hotmail.co.uk>
2017-10-31 09:39:04 +0000

    matts code

--- Similarity.m
+++ Similarity.m
@@ -1,4 +1,4 @@
-function S = Similarity(overlap1,overlap2)
+function S = Similarity(overlap1,overlap2,W)
 % INPUTS:       overlap1    - A matrix of feature space values in the
 %                           overlap of the reference image.
 %               overlap2    - A matrix of feature space values in the
@@ -9,6 +9,8 @@ if      ~prod(size(overlap1)==size(overlap2))
     errorMessage = 'Dimensions of overlaps are not equal.';
     error(errorMessage);
 end
+% S = sum(sum(overlap1.*overlap2))-...
+% sum(overlap1(overlap1~=0 & overlap2==0))-...
+% sum(overlap2(overlap2~=0 & overlap1==0));
 S = sum(sum(overlap1.*overlap2))-...
-    sum(overlap1(overlap1~=0 & overlap2==0))-...
-    sum(overlap2(overlap2~=0 & overlap1==0));
+	W*sum(sum(abs(overlap1-overlap2)));
