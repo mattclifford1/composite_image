a582ad7a4550bad9ea900bcf31683c267841382d
scottncoleman <sc15358@my.bristol.ac.uk>
2017-11-03 10:15:28 +0000

    vectors

--- SeamRemoval.m
+++ SeamRemoval.m
@@ -1,13 +1,13 @@
 function Visibility = SeamRemoval(folder,frames,v,a)
 
-Visibility = 0;
+Visibility = zeros(248,1);
 
-I1 = imread(['noscratch/',frames.fileName{(frames.number==frames.number(1))}]);
-I2 = imread(['noscratch/',frames.fileName{(frames.number==frames.number(2))}]);
+I1 = imread([folder,frames.fileName{(frames.number==frames.number(1))}]);
+I2 = imread([folder,frames.fileName{(frames.number==frames.number(2))}]);
 
 for h = 1:248
     [O1,O2,Vis] = MergeGrad(I1,I2,v(h,:),a);
-    Visibility = Visibility + Vis;
+    Visibility(h) = Vis;
     imwrite(O1,strcat('noSeam',num2str(a),'/',frames.fileName{find(frames.number==h,1)}),'.tif')
     if h~=248
         I1 = O2;
